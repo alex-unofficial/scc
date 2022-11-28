07ca5d85633d5a9a8f9e583bd5d31f5547dd7e10
Alex <athanasiadisalex2@gmail.com>
2022-11-28 14:44:12 +0200

    changed makefile to only build serial
    this is so I can create branches with the different implementations more
    easily

--- Makefile
+++ Makefile
@@ -4,11 +4,16 @@ CFLAGS=-fopenmp -g
 LDFLAGS=-lpthread
 
 # The name of the executable
-PROGRAM=scc
+PROGNAME=scc
 
-# The directory to put object files in and the object files themselves
-OBJDIR=obj
-SRCOBJ=scc.o graph.o scc_serial.o scc_pthreads.o scc_openmp.o
+# the build and bin directories will contain the objects and executables
+BINDIR=bin
+OBJDIR=build
+
+PROGRAM=$(BINDIR)/$(PROGNAME)
+
+# the object files
+SRCOBJ=scc.o graph.o scc_serial.o
 EXTOBJ=mmio.o
 OBJFILES=$(addprefix $(OBJDIR)/,$(SRCOBJ) $(EXTOBJ))
 
@@ -23,12 +28,16 @@ override CFLAGS += $(patsubst %,-I%,$(subst :,$(SPACE),$(VPATH)))
 all: $(PROGRAM)
 
 # Linking the object files into the final executable
-$(PROGRAM): $(OBJFILES)
+$(PROGRAM): $(OBJFILES) | $(BINDIR)
 	$(CC) $(CFLAGS) -o $(PROGRAM) $^ $(LDFLAGS)
 
 # Compiling the C files into object files
 $(OBJDIR)/%.o: %.c | $(OBJDIR)
 	$(CC) -c $(CFLAGS) $< -o $@
+
+# Creating the bin directory
+$(BINDIR):
+	mkdir $(BINDIR)
 
 # Creating the object directory
 $(OBJDIR):
@@ -37,5 +46,5 @@ $(OBJDIR):
 # Remove all the produced files from the directory
 .PHONY: clean
 clean: 
-	rm -rf $(PROGRAM) $(OBJDIR)
+	rm -rf $(BINDIR) $(OBJDIR)
 
--- src/scc/scc.c
+++ src/scc/scc.c
@@ -8,11 +8,6 @@
 
 #include <graph.h>
 #include <scc_serial.h>
-#include <scc_pthreads.h>
-#include <scc_openmp.h>
-
-#define NUMTHREADS 4
-#undef NUMTHREADS
 
 int main(int argc, char **argv) {
 	char* mtx_fname = NULL;
@@ -51,71 +46,7 @@ int main(int argc, char **argv) {
 	printf("total time: %0.4f sec\n", elapsedtime);
 
 	printf("\n");
-
-	printf("=== pthreads SCC algorithm ===\n");
-	vert_t *p_scc_id;
-	clock_gettime(CLOCK_MONOTONIC, &t1);
-	ssize_t p_n_scc = p_scc_coloring(G, &p_scc_id);
-	clock_gettime(CLOCK_MONOTONIC, &t2);
-
-	if(p_n_scc == -1) {
-		free(scc_id);
-		free_graph(G);
-		return -1;
-	}
-
-	printf("number of SCCs = %zd\n", p_n_scc);
-
-	elapsedtime = (t2.tv_sec - t1.tv_sec);
-	elapsedtime += (t2.tv_nsec - t1.tv_nsec) / 1000000000.0;
-	printf("total time: %0.4f sec\n", elapsedtime);
-
-	printf("\n");
-
-	printf("=== openmp SCC algorithm ===\n");
-	vert_t *omp_scc_id;
-	clock_gettime(CLOCK_MONOTONIC, &t1);
-	ssize_t omp_n_scc = omp_scc_coloring(G, &omp_scc_id);
-	clock_gettime(CLOCK_MONOTONIC, &t2);
-
-	if(omp_n_scc == -1) {
-		free(scc_id);
-		free(p_scc_id);
-		free_graph(G);
-		return -1;
-	}
-
-	printf("number of SCCs = %zd\n", omp_n_scc);
-
-	elapsedtime = (t2.tv_sec - t1.tv_sec);
-	elapsedtime += (t2.tv_nsec - t1.tv_nsec) / 1000000000.0;
-	printf("total time: %0.4f sec\n", elapsedtime);
-
-	printf("\n");
-
-	printf("=== checking for errors ===\n");
-	size_t num_errors = 0;
-	if(n_scc != p_n_scc || n_scc != omp_n_scc) {
-		printf(
-			"number of SCCs:\nserial n_scc = %zd, pthread n_scc = %zd, openmp n_scc = %zd\n",
-			n_scc, p_n_scc, omp_n_scc
-		);
-		num_errors++;
-	}
-	for(vert_t i = 0 ; i < G->n_verts ; i++) { 
-		if(scc_id[i] != p_scc_id[i] || scc_id[i] != omp_scc_id[i]) {
-			printf(
-				"at vertex %u: serial scc_id = %u, pthread scc_id = %u\n, openmp scc_id = %u\n",
-				i, scc_id[i], p_scc_id[i], omp_scc_id[i]
-			);
-			num_errors++;
-		}
-	}
-	printf("errors found: %zu\n", num_errors);
-
 	free(scc_id);
-	free(p_scc_id);
-	free(omp_scc_id);
 
 	free_graph(G);
 
